1fa12a4486c0d4921bcf4855c9abb2b77766f00c
Noah Omdal <nomdal@github.com>
2022-03-08 13:30:34 -0800

    updating tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
-class TestMyApp(TestCase):
-    def test_first(self):
-        print("TESTING HERE")
+class YourTestClass(TestCase):
+    def setUp(self):
+        print("Setting up")
+        pass
+
+    def tearDown(self):
+        print("Tearing down")
+        pass
+
+    def test_something_that_will_pass(self):
+        self.assertFalse(False)
+
+    def test_something_that_will_fail(self):
+        self.assertTrue(False)
