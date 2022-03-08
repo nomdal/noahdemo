81c710d2461b3b6e4f43581233350bee2a236bb7
Noah Omdal <nomdal@github.com>
2022-03-08 14:49:02 -0800

    updating tests and greetings action

--- tasks/tests.py
+++ tasks/tests.py
@@ -11,6 +11,3 @@ class YourTestClass(TestCase):
 
     def test_something_that_will_pass(self):
         self.assertFalse(False)
-
-    def test_something_that_will_fail(self):
-        self.assertTrue(True)
